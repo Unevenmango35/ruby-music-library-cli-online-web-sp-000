2d6ff576e2c822f0970fdad208b65c38952cf17a
Rosie Choi <rosiechoimanso@gmail.com>
2021-02-03 22:54:33 +0000

    Automatically backed up by Learn

---
+++ lib/music_importer.rb
@@ -0,0 +1,13 @@
+class MusicImporter
+  attr_accessor :path, :files
+
+  def initialize(path)
+      @path = path
+    end
+
+    def files
+      @files = Dir.glob("#{path}/*.mp3").map { |file| file.gsub("#{path}/", "") }
+
+    end
+
+end
